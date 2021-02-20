4784683bb23194df128a8dd4b5e3e451eba0d9e4
Gokul Sakthi <gokulcode234@gmail.com>
2021-02-20 10:12:00 +0530

    Changed CartITem

--- spring/bookstore-spring/src/main/java/com/example/model/CartItemModel.java
+++ spring/bookstore-spring/src/main/java/com/example/model/CartItemModel.java
@@ -6,31 +6,50 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class CartItemModel extends ProductModel {
+public class CartItemModel {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	String cartItemId;
 	int quantity;
-	
-	public CartItemModel() {
-		// TODO Auto-generated constructor stub
-		super();
+	String productName;
+	double price;
+	String description;
+	public String getCartItemId() {
+		return cartItemId;
 	}
-
+	public void setCartItemId(String cartItemId) {
+		this.cartItemId = cartItemId;
+	}
 	public int getQuantity() {
 		return quantity;
 	}
-
 	public void setQuantity(int quantity) {
-		if(quantity >= 0) {
-			this.quantity = quantity;
-		}	
+		this.quantity = quantity;
 	}
-
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 	@Override
 	public String toString() {
-		return "CartItem [quantity=" + quantity + "]";
+		return "CartItemModel [cartItemId=" + cartItemId + ", quantity=" + quantity + ", productName=" + productName
+				+ ", price=" + price + ", description=" + description + "]";
 	}
 	
 	
