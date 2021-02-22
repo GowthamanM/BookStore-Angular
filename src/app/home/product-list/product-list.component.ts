00db44cc0c8bf210efd6547b1537e177728d4bf9
Gowthaman T <happygowthaman@gmail.com>
2021-02-22 18:52:32 +0530

    Client Home UI Completed

--- src/app/home/product-list/product-list.component.ts
+++ src/app/home/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Productmodel } from 'src/app/model/productmodel';
 import { ProductListService } from './product-list.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-list',
@@ -12,13 +13,31 @@ export class ProductListComponent implements OnInit {
   products:Productmodel[] | undefined;
 
   constructor(private productListService:ProductListService) { 
-    
+    this.getAllProducts();
     
   }
 
   ngOnInit(): void {
-    this.productListService.getProduct().subscribe(data =>console.log(data));
-    console.log(this.products);
+    
+  }
+
+  public getAllProducts() {
+    this.productListService.getProduct()
+    .pipe(map(responseData => {
+      const productsArray = [];
+      for(const key in responseData) {
+        if(responseData.hasOwnProperty(key)) {
+          productsArray.push( { ...responseData[key] })
+        }
+      }
+      return productsArray;
+    }))
+    .subscribe(data => {
+      console.log(data);
+      this.products = data;
+    });
+
+    // console.log(this.products[0].productName);
   }
 
 }
