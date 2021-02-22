e1cf170fef995451346be290852983869c735d6b
GowthamanM <gowthamancode@gmail.com>
2021-02-22 12:38:29 +0530

    adding new Item feature added

--- src/app/admin/addproduct/addproduct.component.ts
+++ src/app/admin/addproduct/addproduct.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Productmodel } from 'src/app/model/productmodel';
+import { AddproductService } from 'src/app/services/addproduct.service';
+import{NgForm} from '@angular/forms'
 @Component({
   selector: 'app-addproduct',
   templateUrl: './addproduct.component.html',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddproductComponent implements OnInit {
 
-  constructor() { }
+  product:Productmodel = new Productmodel;
+  constructor(private addproductService: AddproductService,public router:Router) { }
 
   ngOnInit(): void {
   }
 
+  addproduct(form:NgForm,name:String,price:String,description:String,url:String,quantity:any){
+    this.product.productId = "temp";
+    this.product.productName = name;
+    this.product.price = price;
+    this.product.description = description;
+    this.product.imageUrl = url;
+    this.product.quantity = +quantity;
+    console.log(this.product);
+    
+    this.addproductService.save(this.product).subscribe(()=>this.gotoaddProduct());
+  }
+  gotoaddProduct(){
+    this.router.navigate(['admin']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { AdminComponent } from './admin/admin.component';
